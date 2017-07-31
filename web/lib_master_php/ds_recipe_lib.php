962bb15a04798413ebb09afa6761e871f8e306ff
rfeely <ronan.feely@gmail.com>
2017-07-31 15:41:43 +0100

    Update to replace logic

--- web/lib_master_php/ds_recipe_lib.php
+++ web/lib_master_php/ds_recipe_lib.php
@@ -219,7 +219,7 @@ class DS_recipe_lib {
 			$this->my_url = $this->rm_queryparameters($this->full_url($_SERVER));
 		}
 		error_log(print_r($this->my_url, TRUE)); 
-		str_replace("http","https",$this->my_url);
+		$this->my_url = str_replace("http","https",$this->my_url);
 		error_log(print_r($this->my_url, TRUE)); 
 		return $this->my_url;
 	}
